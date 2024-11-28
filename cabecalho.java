48fec8b11f7bac8873ac9e977599694d93210de9
andrerosa98 <202420120000024@pucgo.edu.br>
2024-11-28 18:13:56 -0300

    criação do menu OpcaoEntradas

--- cabecalho.java
+++ cabecalho.java
@@ -8,11 +8,18 @@ public class cabecalho{
     }
 
     public static void Entradas(){
-        String[] postos = {"Finsocial", "João Bráz", "Matriz", "Climed", "Hospital São Domingos", "Sair"};
+        String[] postos = {"Finsocial", "João Bráz", "Matriz", "Climed", "Hospital São Domingos", "Voltar", "Sair"};
         System.out.println("***** SELEÇÃO DE POSTO *****");
         for (int i = 0; i < postos.length; i++){
             System.out.println((i+1) + " - " + postos[i]);
         }
         System.out.print("Digite o posto desejado: ");
+    }
+    public static void OpcaoEntradas(){
+        String[] opcaoEntradas = {"Dinheiro", "Pix", "Cartão de Débito", "Cartão de Crédito", "Voltar", "Sair"};
+        for (int i = 0; i < opcaoEntradas.length; i++){
+            System.out.println((i+1) + " - " + opcaoEntradas[i]);
+        }
+        System.out.print("Digite a opção desejada: ");
     }
 }
